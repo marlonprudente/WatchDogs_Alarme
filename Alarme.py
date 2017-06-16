078ad20364e2d1b9580e9c0cac4d263ac7f9c138
Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
2017-06-16 17:38:29 -0300

    4

--- Alarme.py
+++ Alarme.py
@@ -85,7 +85,7 @@ def sensores():
 		for row in sql.fetchall():
 			if GPIO.input(janela1):#| janela2 | janela3 | sala1 | quarto1 | quarto2):
 				sql.execute('UPDATE sensors SET status=2 WHERE status=1')
-				con.commit()   
+				#con.commit()   
 
 def led():
     global start
