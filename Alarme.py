4e097deb0fbac9503cd8eadf120ba9463e04d5a6
Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
2017-06-17 12:06:51 -0300

    a

--- Alarme.py
+++ Alarme.py
@@ -62,13 +62,13 @@ def desativar():
 			
 def sensores():
 	sql = con.cursor()
-	global ativar
-	
+	global ativar	
 	while True:	
 			if GPIO.input(janela1) | GPIO.input(janela2) | GPIO.input(janela3) | GPIO.input(sala1) | GPIO.input(quarto1) | GPIO.input(quarto2):
 				sql.execute('UPDATE sensors SET status=2 WHERE status = 1')
 				con.commit()   
 				disparar()
+			sql.execute('SELECT nome, status FROM sensors')
 			for row in sql.fetchall():
 				if (int(row[1]) == -1 and ativar==1):
 					desativar()
